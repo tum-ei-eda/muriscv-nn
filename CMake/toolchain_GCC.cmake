9562f4be71e2f6c716db2247b646a4aa584dd7ce
Fabian Peddinghaus <fabianpedd@gmail.com>
2022-08-30 05:53:22 +0200

    refactor CMake build system

--- CMake/toolchain_GCC.cmake
+++ CMake/toolchain_GCC.cmake
@@ -18,32 +18,10 @@
 
 # Contains toolchain configurations and settings for using GCC
 
-# The Generic system name is used for bare-metal targets (without OS) in CMake
-set(CMAKE_SYSTEM_NAME Generic)
-
-# Fully featured RISC-V core with vector and packed extension
-set(CMAKE_SYSTEM_PROCESSOR rv32gcvp)
-
-# Lets stick to standard .elf file ending for now
-set(CMAKE_EXECUTABLE_SUFFIX_C .elf)
-set(CMAKE_EXECUTABLE_SUFFIX_CXX .elf)
-
-# Path to your RISC-V GCC compiler supporting the vector extension
+# Path to your RISC-V GCC compiler
 set(RISCV_GCC_PREFIX "/opt/riscv" CACHE PATH "Install location of GCC RISC-V toolchain.")
 set(RISCV_GCC_BASENAME "riscv32-unknown-elf" CACHE STRING "Base name of the toolchain executables.")
 set(TC_PREFIX "${RISCV_GCC_PREFIX}/bin/${RISCV_GCC_BASENAME}-")
-
-# Set the desired architecture and application binary interface
-# For more info on these, see here https://www.sifive.com/blog/all-aboard-part-1-compiler-args
-if(USE_VEXT)
-  set(RISCV_ARCH "rv32gcv" CACHE STRING "march argument to the compiler, using the vector extension")
-elseif(USE_PEXT)
-  set(RISCV_ARCH "rv32gcp" CACHE STRING "march argument to the compiler, using the packed extension")
-else()
-  set(RISCV_ARCH "rv32gc" CACHE STRING "march argument to the compiler, using only scalar instructions")
-endif()
-
-set(RISCV_ABI "ilp32d" CACHE STRING "mabi argument to the compiler")
 
 set(CMAKE_C_COMPILER ${TC_PREFIX}gcc)
 set(CMAKE_CXX_COMPILER ${TC_PREFIX}g++)
@@ -55,7 +33,8 @@ set(CMAKE_AR ${TC_PREFIX}ar)
 set(CMAKE_RANLIB ${TC_PREFIX}ranlib)
 set(CMAKE_STRIP ${TC_PREFIX}strip)
 
-add_definitions(-march=${RISCV_ARCH})
-add_definitions(-mabi=${RISCV_ABI})
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=${RISCV_ARCH} -mabi=${RISCV_ABI}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=${RISCV_ARCH} -mabi=${RISCV_ABI}")
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -march=${RISCV_ARCH} -mabi=${RISCV_ABI}")
 
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -march=${RISCV_ARCH} -mabi=${RISCV_ABI}")
--- CMake/toolchain_x86.cmake
+++ CMake/toolchain_x86.cmake
@@ -19,8 +19,4 @@
 # Contains toolchain configurations and settings for using GCC on x86
 
 set(CMAKE_SYSTEM_NAME Linux)
-
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
-
-set(CMAKE_EXECUTABLE_SUFFIX_C .elf)
-set(CMAKE_EXECUTABLE_SUFFIX_CXX .elf)
