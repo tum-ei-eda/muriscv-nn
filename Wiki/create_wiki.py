2931ca363e0e5460b31d7bc41fa3cc55d0149fe1
Philipp van Kempen <philipp.van-kempen@tum.de>
2023-02-20 13:44:09 +0100

    lint python code

--- Wiki/create_wiki.py
+++ Wiki/create_wiki.py
@@ -7,28 +7,28 @@ from datetime import date
 today = date.today()
 
 
-with open("aww-benchmark-"+str(today)+"/out.csv", newline="") as file:
+with open("aww-benchmark-" + str(today) + "/out.csv", newline="") as file:
     aww_csv = list(csv.reader(file))
-with open("vww-benchmark-"+str(today)+"/out.csv", newline="") as file:
+with open("vww-benchmark-" + str(today) + "/out.csv", newline="") as file:
     vww_csv = list(csv.reader(file))
-with open("resnet-benchmark-"+str(today)+"/out.csv", newline="") as file:
+with open("resnet-benchmark-" + str(today) + "/out.csv", newline="") as file:
     resnet_csv = list(csv.reader(file))
-with open("toycar-benchmark-"+str(today)+"/out.csv", newline="") as file:
+with open("toycar-benchmark-" + str(today) + "/out.csv", newline="") as file:
     toycar_csv = list(csv.reader(file))
-    
-    
+
+
 def env_var(value, key):
-  return os.getenv(key, value)
+    return os.getenv(key, value)
+
 
 environment = jinja2.Environment(loader=FileSystemLoader("./"))
 
-environment.filters['env_var'] = env_var
+environment.filters["env_var"] = env_var
 
 template = environment.get_template("benchmarks_template.md")
 
 
-
-filename = f"Benchmarks-"+str(today)+".md"
+filename = f"Benchmarks-" + str(today) + ".md"
 content = template.render(aww_data=aww_csv, vww_data=vww_csv, resnet_data=resnet_csv, toycar_data=toycar_csv)
 with open(filename, mode="w", encoding="utf-8") as message:
     message.write(content)
