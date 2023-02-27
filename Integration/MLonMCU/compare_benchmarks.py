e967b8fa0298db02657ab77cb5ba0404b170d496
ParkerJones567 <parker.jones@tum.de>
2023-02-27 13:31:34 +0100

    Update compare_benchmarks.py for new MIPS column

--- Integration/MLonMCU/compare_benchmarks.py
+++ Integration/MLonMCU/compare_benchmarks.py
@@ -39,11 +39,11 @@ def main():
                     output_csv.append(reference)
                     output_csv.append(benchmark)
                     
-                    difference = [ '', '', '', '', '', '', '', '', int(reference[8])-int(benchmark[8]), int(reference[9])-int(benchmark[9]), int(reference[10])-int(benchmark[10]), int(reference[11])-int(benchmark[11]), int(reference[12])-int(benchmark[12]), int(reference[13])-int(benchmark[13]), int(reference[14])-int(benchmark[14]), int(reference[15])-int(benchmark[15]), '', '', '', '']
+                    difference = [ '', '', '', '', '', '', '', '', int(reference[8])-int(benchmark[8]), float(reference[9])-float(benchmark[9]), int(reference[10])-int(benchmark[10]), int(reference[11])-int(benchmark[11]), int(reference[12])-int(benchmark[12]), int(reference[13])-int(benchmark[13]), int(reference[14])-int(benchmark[14]), int(reference[15])-int(benchmark[15]), int(reference[16])-int(benchmark[16]), '', '', '', '']
                     
                     output_csv.append(difference)
                     
-                    percent_diff = [ '', '', '', '', '', '', '', '', float(difference[8])/float(reference[8]) * 100, float(difference[9])/float(reference[9]) * 100, float(difference[10])/float(reference[10]) * 100, float(difference[11])/float(reference[11]) * 100, float(difference[12])/float(reference[12]) * 100, float(difference[13])/float(reference[13]) * 100, float(difference[14])/float(reference[14]) * 100, float(difference[15])/float(reference[15]) * 100, '', '', '', '']
+                    percent_diff = [ '', '', '', '', '', '', '', '', float(difference[8])/float(reference[8]) * 100, '', float(difference[10])/float(reference[10]) * 100, float(difference[11])/float(reference[11]) * 100, float(difference[12])/float(reference[12]) * 100, float(difference[13])/float(reference[13]) * 100, float(difference[14])/float(reference[14]) * 100, float(difference[15])/float(reference[15]) * 100, float(difference[16])/float(reference[16]) * 100, '', '', '', '']
                     
                     output_csv.append(percent_diff)
                     
