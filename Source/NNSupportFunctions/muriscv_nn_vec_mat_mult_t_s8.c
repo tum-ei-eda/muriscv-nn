bd6440d54eb5e9eec0e69ea8c71fd394ce1a622e
ParkerJones567 <parker.jones@tum.de>
2024-02-21 20:17:56 +0100

    Improve VEXT performance for toycar

--- Source/NNSupportFunctions/muriscv_nn_vec_mat_mult_t_s8.c
+++ Source/NNSupportFunctions/muriscv_nn_vec_mat_mult_t_s8.c
@@ -61,7 +61,6 @@ muriscv_nn_status muriscv_nn_vec_mat_mult_t_s8(const q7_t *lhs,
                                                const int32_t activation_max,
                                                const int32_t address_offset)
 {
-
 #if defined(USE_VEXT)
 
     /* At some point in time there might be implementations with the Zvediv extension.
@@ -70,8 +69,8 @@ muriscv_nn_status muriscv_nn_vec_mat_mult_t_s8(const q7_t *lhs,
      */
 
     /* TODO: This code might benefit from reducing the group size and instead operating on
-     * more than one row at a time (for example 2x unroll of outer loop).
-     * When rhs_cols_cnt << vlmax, then this code currently gives very bad performance...
+     * more than one row at a time (for example 2x unroll of outer loopthis code currently gives very bad performance..).
+     * When rhs_cols_cnt << vlmax, then .
      * Similiar to here:
      * https://github.com/xianyi/OpenBLAS/blob/2d7ca63e21e97dcd2db5c3aa7658f6cc2a504e55/kernel/riscv64/axpy_vector.c#L145
      * Maybe use the vsetvli function to prevent the need for extra code to handle the leftover at the end!?
@@ -86,8 +85,123 @@ muriscv_nn_status muriscv_nn_vec_mat_mult_t_s8(const q7_t *lhs,
     /* Also have a look here and compare:
      * https://github.com/xianyi/OpenBLAS/blob/2d7ca63e21e97dcd2db5c3aa7658f6cc2a504e55/kernel/riscv64/gemv_n_vector.c#L98
      */
-    for (int i_loop_cnt = 0; i_loop_cnt < rhs_rows; i_loop_cnt++)
+    const int32_t row_loop_cnt = rhs_rows / 5;
+    for (int i_loop_cnt = 0; i_loop_cnt < row_loop_cnt; i_loop_cnt++)
     {
+
+        const q7_t *lhs_ptr = &lhs[0];
+        const q7_t *rhs_ptr_0 = &rhs[0];
+        const q7_t *rhs_ptr_1 = &rhs[rhs_cols];
+        const q7_t *rhs_ptr_2 = &rhs[rhs_cols*2];
+        const q7_t *rhs_ptr_3 = &rhs[rhs_cols*3];
+        const q7_t *rhs_ptr_4 = &rhs[rhs_cols*4];
+
+
+        volatile size_t vl = vsetvl_e32m4(rhs_cols);
+        vint32m4_t result_0 = vmv_v_x_i32m4(0, vl);
+        vint32m4_t result_1 = vmv_v_x_i32m4(0, vl);
+        vint32m4_t result_2 = vmv_v_x_i32m4(0, vl);
+        vint32m4_t result_3 = vmv_v_x_i32m4(0, vl);
+        vint32m4_t result_4 = vmv_v_x_i32m4(0, vl);
+
+        int32_t rhs_cols_cnt = rhs_cols;
+        while (rhs_cols_cnt > 0)
+        {
+            vl = vsetvl_e32m4(rhs_cols_cnt);
+
+            vint32m4_t lhs_value =  __riscv_vsext_vf4_i32m4( __riscv_vle8_v_i8m1(lhs_ptr, vl), vl);
+            lhs_value = vadd_vx_i32m4(lhs_value, lhs_offset, vl);
+
+            vint32m4_t rhs_value_0 =  __riscv_vsext_vf4_i32m4( __riscv_vle8_v_i8m1(rhs_ptr_0, vl), vl);
+            result_0 = __riscv_vmacc_vv_i32m4(result_0, lhs_value, rhs_value_0, vl);
+
+            vint32m4_t rhs_value_1 = __riscv_vsext_vf4_i32m4( __riscv_vle8_v_i8m1(rhs_ptr_1, vl), vl);
+            result_1 = __riscv_vmacc_vv_i32m4(result_1, lhs_value, rhs_value_1, vl);
+
+            vint32m4_t rhs_value_2 = __riscv_vsext_vf4_i32m4( __riscv_vle8_v_i8m1(rhs_ptr_2, vl), vl);
+            result_2 = __riscv_vmacc_vv_i32m4(result_2, lhs_value, rhs_value_2, vl);
+
+            vint32m4_t rhs_value_3 = __riscv_vsext_vf4_i32m4( __riscv_vle8_v_i8m1(rhs_ptr_3, vl), vl);
+            result_3 = __riscv_vmacc_vv_i32m4(result_3, lhs_value, rhs_value_3, vl);
+
+            vint32m4_t rhs_value_4 = __riscv_vsext_vf4_i32m4( __riscv_vle8_v_i8m1(rhs_ptr_4, vl), vl);
+            result_4 = __riscv_vmacc_vv_i32m4(result_4, lhs_value, rhs_value_4, vl);
+
+            rhs_ptr_0 += vl;
+            rhs_ptr_1 += vl;
+            rhs_ptr_2 += vl;
+            rhs_ptr_3 += vl;
+            rhs_ptr_4 += vl;
+            lhs_ptr += vl;
+            rhs_cols_cnt -= vl;
+        }
+
+        vl = vsetvl_e32m1(1); // TODO(fabianpedd): Is this really needed?
+        vint32m1_t reduct_0 = __riscv_vundefined_i32m1();
+        vint32m1_t reduct_1 = __riscv_vundefined_i32m1();
+        vint32m1_t reduct_2 = __riscv_vundefined_i32m1();
+        vint32m1_t reduct_3 = __riscv_vundefined_i32m1();
+        vint32m1_t reduct_4 = __riscv_vundefined_i32m1();
+
+        if (bias)
+        {
+            reduct_0 = vle32_v_i32m1(bias++, vl);
+            reduct_1 = vle32_v_i32m1(bias++, vl);
+            reduct_2 = vle32_v_i32m1(bias++, vl);
+            reduct_3 = vle32_v_i32m1(bias++, vl);
+            reduct_4 = vle32_v_i32m1(bias++, vl);
+        }
+        else
+        {
+            reduct_0 = vmv_v_x_i32m1(0, vl);
+            reduct_1 = vmv_v_x_i32m1(0, vl);
+            reduct_2 = vmv_v_x_i32m1(0, vl);
+            reduct_3 = vmv_v_x_i32m1(0, vl);
+            reduct_4 = vmv_v_x_i32m1(0, vl);
+        }
+
+
+        vl = vsetvl_e32m4(rhs_cols);
+        
+        reduct_0 = __riscv_vredsum_vs_i32m4_i32m1(result_0, reduct_0, vl);
+        reduct_1 = __riscv_vredsum_vs_i32m4_i32m1(result_1, reduct_1, vl);
+        reduct_2 = __riscv_vredsum_vs_i32m4_i32m1(result_2, reduct_2, vl);
+        reduct_3 = __riscv_vredsum_vs_i32m4_i32m1(result_3, reduct_3, vl);
+        reduct_4 = __riscv_vredsum_vs_i32m4_i32m1(result_4, reduct_4, vl);
+        
+
+        vl = vsetvl_e32m2(5);
+        vint32m2_t output =  __riscv_vlmul_ext_v_i32m1_i32m2(reduct_0);
+        output = __riscv_vslideup_vx_i32m2(output, __riscv_vlmul_ext_v_i32m1_i32m2(reduct_1), (size_t)1, vl);
+        output = __riscv_vslideup_vx_i32m2(output, __riscv_vlmul_ext_v_i32m1_i32m2(reduct_2), (size_t)2, vl);
+        output = __riscv_vslideup_vx_i32m2(output, __riscv_vlmul_ext_v_i32m1_i32m2(reduct_3), (size_t)3, vl);
+        output = __riscv_vslideup_vx_i32m2(output, __riscv_vlmul_ext_v_i32m1_i32m2(reduct_4), (size_t)4, vl);
+
+
+        //Requantize and clip
+        vint32m2_t mult_vec = vmv_v_x_i32m2(dst_multiplier, vl);
+        vint32m2_t shift_vec = vmv_v_x_i32m2(dst_shift, vl);
+        output = muriscv_nn_requantize_vint32m2(output, mult_vec, shift_vec, vl);
+        output = vadd_vx_i32m2(output, dst_offset, vl);
+        output = vmax_vx_i32m2(output, activation_min, vl);
+        output = vmin_vx_i32m2(output, activation_max, vl);
+
+        //reduction to 8 bit
+        vint16m1_t output_16 = __riscv_vundefined_i16m1();
+        output_16 = __riscv_vncvt_x_x_w_i16m1(output, vl);
+        vint8mf2_t output_8 = __riscv_vundefined_i8mf2();
+        output_8 = __riscv_vncvt_x_x_w_i8mf2(output_16, vl);
+        //store
+        __riscv_vse8_v_i8mf2(dst, output_8, vl);
+
+        dst += address_offset * 5;
+        rhs += rhs_cols * 5;
+    }
+    //Handle Remaining Rows
+    const int loop_cnt = rhs_rows % 5;
+    for (int i_loop_cnt = 0; i_loop_cnt < loop_cnt; i_loop_cnt++)
+    {
+
         const q7_t *lhs_ptr = &lhs[0];
         const q7_t *rhs_ptr = &rhs[0];
 
@@ -134,6 +248,7 @@ muriscv_nn_status muriscv_nn_vec_mat_mult_t_s8(const q7_t *lhs,
         *dst = (q7_t)res_scalar;
         dst += address_offset;
         rhs += rhs_cols;
+        
     }
 
 #elif defined(USE_PEXT) /* defined(USE_VEXT) */
@@ -416,49 +531,66 @@ muriscv_nn_status muriscv_nn_vec_mat_mult_t_s8(const q7_t *lhs,
 
 #else /* defined(USE_PEXT) */
 
-    /* Uses 3x loop unrolling in order to expose more ILP */
-    const int32_t row_loop_cnt = rhs_rows / 3;
+    /* Uses 5x loop unrolling in order to expose more ILP */
+    const int32_t row_loop_cnt = rhs_rows / 5;
     for (int i_row_loop_cnt = 0; i_row_loop_cnt < row_loop_cnt; i_row_loop_cnt++)
     {
+        
         const q7_t *lhs_ptr = lhs;
         const q7_t *rhs_ptr_0 = &rhs[0];
         const q7_t *rhs_ptr_1 = &rhs[rhs_cols];
         const q7_t *rhs_ptr_2 = &rhs[rhs_cols * 2];
-
+        const q7_t *rhs_ptr_3 = &rhs[rhs_cols * 3];
+        const q7_t *rhs_ptr_4 = &rhs[rhs_cols * 4];
+        
         q31_t res00 = 0;
         q31_t res01 = 0;
         q31_t res02 = 0;
+        q31_t res03 = 0;
+        q31_t res04 = 0;
         if (bias)
         {
             res00 = *bias++;
             res01 = *bias++;
             res02 = *bias++;
+            res03 = *bias++;
+            res04 = *bias++;
         }
         for (int32_t rhs_cols_idx = 0; rhs_cols_idx < rhs_cols; ++rhs_cols_idx)
         {
             const q31_t rhs_value0 = (int8_t)*rhs_ptr_0;
             const q31_t rhs_value1 = (int8_t)*rhs_ptr_1;
             const q31_t rhs_value2 = (int8_t)*rhs_ptr_2;
+            const q31_t rhs_value3 = (int8_t)*rhs_ptr_3;
+            const q31_t rhs_value4 = (int8_t)*rhs_ptr_4;
             const q31_t lhs_value = (int8_t)*lhs_ptr + lhs_offset;
 
             res00 += lhs_value * rhs_value0;
             res01 += lhs_value * rhs_value1;
             res02 += lhs_value * rhs_value2;
+            res03 += lhs_value * rhs_value3;
+            res04 += lhs_value * rhs_value4;
 
             ++rhs_ptr_0;
             ++rhs_ptr_1;
             ++rhs_ptr_2;
+            ++rhs_ptr_3;
+            ++rhs_ptr_4;
             ++lhs_ptr;
         }
         // Quantize down
         res00 = muriscv_nn_requantize(res00, dst_multiplier, dst_shift);
         res01 = muriscv_nn_requantize(res01, dst_multiplier, dst_shift);
         res02 = muriscv_nn_requantize(res02, dst_multiplier, dst_shift);
+        res03 = muriscv_nn_requantize(res03, dst_multiplier, dst_shift);
+        res04 = muriscv_nn_requantize(res04, dst_multiplier, dst_shift);
 
         // Add offset
         res00 += dst_offset;
         res01 += dst_offset;
         res02 += dst_offset;
+        res03 += dst_offset;
+        res04 += dst_offset;
 
         // Clamp the result
         res00 = MAX(res00, activation_min);
@@ -467,19 +599,27 @@ muriscv_nn_status muriscv_nn_vec_mat_mult_t_s8(const q7_t *lhs,
         res01 = MIN(res01, activation_max);
         res02 = MAX(res02, activation_min);
         res02 = MIN(res02, activation_max);
+        res03 = MAX(res03, activation_min);
+        res03 = MIN(res03, activation_max);
+        res04 = MAX(res04, activation_min);
+        res04 = MIN(res04, activation_max);
 
         *dst = (q7_t)res00;
         *(dst + address_offset) = (q7_t)res01;
         *(dst + 2 * address_offset) = (q7_t)res02;
-        dst += 3 * address_offset;
+        *(dst + 3 * address_offset) = (q7_t)res03;
+        *(dst + 4 * address_offset) = (q7_t)res04;
+        dst += 5 * address_offset;
 
-        rhs += 3 * rhs_cols;
+        rhs += 5 * rhs_cols;
+        
     }
 
-    /* Handle the leftover part from 3x loop unrolling */
-    const int loop_cnt = rhs_rows % 3;
+    /* Handle the leftover part from 5x loop unrolling */
+    const int loop_cnt = rhs_rows % 5;
     for (int i_loop_cnt = 0; i_loop_cnt < loop_cnt; i_loop_cnt++)
     {
+        
         const q7_t *lhs_ptr = &lhs[0];
         const q7_t *rhs_ptr = &rhs[0];
 
@@ -513,6 +653,7 @@ muriscv_nn_status muriscv_nn_vec_mat_mult_t_s8(const q7_t *lhs,
         *dst = (int8_t)res00;
         dst += address_offset;
         rhs += rhs_cols;
+        
     }
 
 #endif /* defined(USE_PEXT) */
