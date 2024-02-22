9ed9c94d0f5e65a13fcb94e445dd15d11647b328
ParkerJones567 <parker.jones@tum.de>
2024-02-22 17:34:01 +0100

    Improve vreg usage for CNN benchmarks

--- Source/NNSupportFunctions/muriscv_nn_mat_mul_core_4x_s8.c
+++ Source/NNSupportFunctions/muriscv_nn_mat_mul_core_4x_s8.c
@@ -53,6 +53,7 @@ int8_t *muriscv_nn_mat_mul_core_4x_s8(const int32_t row_elements,
                                       const int32_t *bias,
                                       int8_t *output)
 {
+
 #if defined(USE_VEXT)
 
     for (int i = 0; i < out_ch; i++)
@@ -63,69 +64,66 @@ int8_t *muriscv_nn_mat_mul_core_4x_s8(const int32_t row_elements,
         const int8_t *ip_row_2 = row_base + (2 * offset);
         const int8_t *ip_row_3 = row_base + (3 * offset);
 
-        volatile size_t vl = vsetvl_e32m2(row_elements);
-        vint32m2_t sum_tmp_v = vmv_v_x_i32m2(0, vl);
-        vint32m2_t acc_n0_v = vmv_v_x_i32m2(0, vl);
-        vint32m2_t acc_n1_v = vmv_v_x_i32m2(0, vl);
-        vint32m2_t acc_n2_v = vmv_v_x_i32m2(0, vl);
-        vint32m2_t acc_n3_v = vmv_v_x_i32m2(0, vl);
+        volatile size_t vl = vsetvl_e32m4(row_elements);
+        vint32m4_t sum_tmp_v = vmv_v_x_i32m4(0, vl);
+        vint32m4_t acc_n0_v = vmv_v_x_i32m4(0, vl);
+        vint32m4_t acc_n1_v = vmv_v_x_i32m4(0, vl);
+        vint32m4_t acc_n2_v = vmv_v_x_i32m4(0, vl);
+        vint32m4_t acc_n3_v = vmv_v_x_i32m4(0, vl);
+
 
         size_t loop_cnt = row_elements;
         size_t loop_ptr = 0;
         while (loop_cnt > 0)
         {
-            vl = vsetvl_e32m2(loop_cnt);
+            vl = vsetvl_e32m4(loop_cnt);
 
-            vint32m2_t col_val = vsext_vf4_i32m2(vle8_v_i8mf2(col_base + loop_ptr, vl), vl);
-            sum_tmp_v = vmacc_vv_i32m2(sum_tmp_v, col_val, vmv_v_x_i32m2(1, vl), vl);  // TODO(fabianpedd): Should be a tail undisturbed add
+            vint32m4_t col_val = __riscv_vsext_vf4_i32m4(__riscv_vle8_v_i8m1(col_base + loop_ptr, vl), vl);
+            sum_tmp_v = __riscv_vmacc_vv_i32m4(sum_tmp_v, col_val, __riscv_vmv_v_x_i32m4(1, vl), vl);  // TODO(fabianpedd): Should be a tail undisturbed add
 
-            vint32m2_t row_val = vsext_vf4_i32m2(vle8_v_i8mf2(ip_row_0 + loop_ptr, vl), vl);
-            acc_n0_v = vmacc_vv_i32m2(acc_n0_v, col_val, row_val, vl);
+            vint32m4_t row_val = __riscv_vsext_vf4_i32m4(__riscv_vle8_v_i8m1(ip_row_0 + loop_ptr, vl), vl);
+            acc_n0_v = __riscv_vmacc_vv_i32m4(acc_n0_v, col_val, row_val, vl);
 
-            row_val = vsext_vf4_i32m2(vle8_v_i8mf2(ip_row_1 + loop_ptr, vl), vl);
-            acc_n1_v = vmacc_vv_i32m2(acc_n1_v, col_val, row_val, vl);
+            row_val = __riscv_vsext_vf4_i32m4(__riscv_vle8_v_i8m1(ip_row_1 + loop_ptr, vl), vl);
+            acc_n1_v = __riscv_vmacc_vv_i32m4(acc_n1_v, col_val, row_val, vl);
 
-            row_val = vsext_vf4_i32m2(vle8_v_i8mf2(ip_row_2 + loop_ptr, vl), vl);
-            acc_n2_v = vmacc_vv_i32m2(acc_n2_v, col_val, row_val, vl);
+            row_val = __riscv_vsext_vf4_i32m4(__riscv_vle8_v_i8m1(ip_row_2 + loop_ptr, vl), vl);
+            acc_n2_v = __riscv_vmacc_vv_i32m4(acc_n2_v, col_val, row_val, vl);
 
-            row_val = vsext_vf4_i32m2(vle8_v_i8mf2(ip_row_3 + loop_ptr, vl), vl);
-            acc_n3_v = vmacc_vv_i32m2(acc_n3_v, col_val, row_val, vl);
+            row_val = __riscv_vsext_vf4_i32m4(__riscv_vle8_v_i8m1(ip_row_3 + loop_ptr, vl), vl);
+            acc_n3_v = __riscv_vmacc_vv_i32m4(acc_n3_v, col_val, row_val, vl);
 
             loop_cnt -= vl;
             loop_ptr += vl;
         }
 
-        vl = vsetvl_e32m2(row_elements);
+        vl = vsetvl_e32m4(row_elements);
 
-        vint32m1_t reduct = vmv_v_x_i32m1(0, vl);
-        reduct = vredsum_vs_i32m2_i32m1(reduct, acc_n0_v, reduct, vl);
-        int32_t acc_n0 = vmv_x_s_i32m1_i32(reduct);
+        vint32m1_t reduct_0 = vmv_v_x_i32m1(0, vl);
+        reduct_0 = __riscv_vredsum_vs_i32m4_i32m1(sum_tmp_v, reduct_0, vl);
+        int32_t sum_tmp = vmv_x_s_i32m1_i32(reduct_0);
 
-        reduct = vmv_v_x_i32m1(0, vl);
-        reduct = vredsum_vs_i32m2_i32m1(reduct, acc_n1_v, reduct, vl);
-        int32_t acc_n1 = vmv_x_s_i32m1_i32(reduct);
+        reduct_0 = vmv_v_x_i32m1(0, vl);
+        reduct_0 = __riscv_vredsum_vs_i32m4_i32m1(acc_n0_v, reduct_0, vl);
 
-        reduct = vmv_v_x_i32m1(0, vl);
-        reduct = vredsum_vs_i32m2_i32m1(reduct, acc_n2_v, reduct, vl);
-        int32_t acc_n2 = vmv_x_s_i32m1_i32(reduct);
+        vint32m1_t reduct_1 = vmv_v_x_i32m1(0, vl);
+        reduct_1 = __riscv_vredsum_vs_i32m4_i32m1(acc_n1_v, reduct_1, vl);
 
-        reduct = vmv_v_x_i32m1(0, vl);
-        reduct = vredsum_vs_i32m2_i32m1(reduct, acc_n3_v, reduct, vl);
-        int32_t acc_n3 = vmv_x_s_i32m1_i32(reduct);
+        vint32m1_t reduct_2 = vmv_v_x_i32m1(0, vl);
+        reduct_2 = __riscv_vredsum_vs_i32m4_i32m1(acc_n2_v, reduct_2, vl);
 
-        reduct = vmv_v_x_i32m1(0, vl);
-        reduct = vredsum_vs_i32m2_i32m1(reduct, sum_tmp_v, reduct, vl);
-        int32_t sum_tmp = vmv_x_s_i32m1_i32(reduct);
+        vint32m1_t reduct_3 = vmv_v_x_i32m1(0, vl);
+        reduct_3 = __riscv_vredsum_vs_i32m4_i32m1(acc_n3_v, reduct_3, vl);
+
 
         vl = vsetvl_e32m4(4);
-        vint32m4_t acc_vec = vmv_v_x_i32m4(0, vl);
-        acc_vec = vslide1up_vx_i32m4(acc_vec, acc_n3, vl);
-        acc_vec = vslide1up_vx_i32m4(acc_vec, acc_n2, vl);
-        acc_vec = vslide1up_vx_i32m4(acc_vec, acc_n1, vl);
-        acc_vec = vslide1up_vx_i32m4(acc_vec, acc_n0, vl);
+        vint32m4_t acc_vec =  __riscv_vlmul_ext_v_i32m1_i32m4(reduct_0);
+        acc_vec = __riscv_vslideup_vx_i32m4(acc_vec, __riscv_vlmul_ext_v_i32m1_i32m4(reduct_1), (size_t)1, vl);
+        acc_vec = __riscv_vslideup_vx_i32m4(acc_vec, __riscv_vlmul_ext_v_i32m1_i32m4(reduct_2), (size_t)2, vl);
+        acc_vec = __riscv_vslideup_vx_i32m4(acc_vec, __riscv_vlmul_ext_v_i32m1_i32m4(reduct_3), (size_t)3, vl);
 
-        int32_t res[4] = {0};
-        vse32_v_i32m4(res, acc_vec, vl);
+        //int32_t res[4] = {0}; //Why are these here?
+        //vse32_v_i32m4(res, acc_vec, vl);
 
         sum_tmp *= conv_params->input_offset;
         if (bias)
