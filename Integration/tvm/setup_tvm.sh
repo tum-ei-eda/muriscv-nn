221515bc89e85a1821053ae4a3cb3706b3c08a2e
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-09-23 00:39:49 +0200

    setup_tvm.sh: fix bug

--- Integration/tvm/setup_tvm.sh
+++ Integration/tvm/setup_tvm.sh
@@ -34,6 +34,7 @@ python3 -m venv .venv
 source .venv/bin/activate
 # pip install numpy==1.26.4
 pip install numpy==1.24.4
+TVM_VERSION=${TVM_VERSION:-stable}
 if [[ "$TVM_VERSION" == "stable" ]]
 then
   pip install apache-tvm
@@ -43,7 +44,7 @@ then
 elif [[ "$TVM_VERSION" != "" ]]
 then
   pip install apache-tvm==$TVM_VERSION --pre
-else
+else  # same as stable
   pip install apache-tvm
 fi
 pip install -r requirements.txt
