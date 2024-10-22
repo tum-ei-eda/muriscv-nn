5b4096992f6a1eaddcac07dc4606007c26c09ef0
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-10-22 10:19:55 +0200

    Scripts/build.sh: update rv32im_zve32x -> rv32im_zicsr_zifencei_zve32x [skip ci]

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -137,7 +137,7 @@ elif [ "${USE_IMV}" == "ON" ];then
       ./download_rv32im_zve32x.sh
     )
   fi
-  IMV_FLAGS="-DRISCV_ARCH=rv32im_zve32x -DRISCV_ABI=ilp32"
+  IMV_FLAGS="-DRISCV_ARCH=rv32im_zicsr_zifencei_zve32x -DRISCV_ABI=ilp32"
 
 elif [ "${TOOLCHAIN}" == "x86" ];then
   echo "Skipping download of RISC-V toolchain for x86 mode."
