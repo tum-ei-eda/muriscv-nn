5ff40bac2b63460b886523292ea542f5a46e5ebb
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-11-25 07:49:50 +0100

    wiki: add Wiki/get_prev_bench_files.sh [skip ci]

---
+++ Wiki/get_prev_bench_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+BASE=$1
+LIMIT=${2:-10000000}
+
+
+BASE_DATE=$(echo $BASE | cut -d'-' -f2-4)
+# echo BASE_DATE=$BASE_DATE
+REST=$(echo $BASE | cut -d'-' -f5-)
+# echo REST=$REST
+FILES=$(ls Benchmarks-*-$REST | grep -v $BASE_DATE | sort -r | head -$LIMIT)
+# echo FILES=$FILES
+echo $FILES
